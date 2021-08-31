c4299944faa328f8b1f94a842d3a25af8194c832
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2021-08-31 10:46:37 +0100

    Finished toggle & hambourger

---
+++ bad.js
@@ -0,0 +1,10 @@
+
+    var list3 = document.getElementById("list3");
+    function showMenu() {
+        list3.style.marginLeft = "30px";
+        }
+    function hideMenu() {
+        list3.style.marginLeft = "380px"
+    }
+
+
