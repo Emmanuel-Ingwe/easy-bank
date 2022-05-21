de91a671955d96333f8c47cc7df0edf4b046052c
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-05-21 23:39:57 +0200

    +

--- bad.js
+++ bad.js
@@ -1,11 +1,10 @@
 
-    var list3 = document.getElementById("list3");
-    function showMenu() {
-        list3.style.marginLeft = "30px";
-        }
-    function hideMenu() {
-        list3.style.marginLeft = "380px"
-    }
+var list3 = document.getElementById("list3");
+function showMenu() {
+    list3.style.marginLeft = "30px";
+}
+function hideMenu() {
+    list3.style.marginLeft = "410px";
+}
 
 
- 
